121ee485dc64065ee8cb787338b331d794c10356
Jayesh Nachnani <jayesh.nachnani@gmail.com>
2021-03-03 18:28:23 +0530

    HypermediaAutoConfiguration change

--- src/main/kotlin/com/finance/deals/DealsApplication.kt
+++ src/main/kotlin/com/finance/deals/DealsApplication.kt
@@ -2,10 +2,12 @@ package com.finance.deals
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration
 import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = arrayOf(HypermediaAutoConfiguration::class))
 class DealsApplication
 
 fun main(args: Array<String>) {
